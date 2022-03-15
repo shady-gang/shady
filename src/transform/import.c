7be60f358b0a927c5c43afe1b524f92e1ad253e6
Hugo Devillers <devillers@cg.uni-saarland.de>
2022-03-15 12:03:44 +0100

    add missing import.c

---
+++ src/transform/import.c
@@ -0,0 +1,26 @@
+#include "ir.h"
+
+const Node* import_node(IrArena* arena, const Node* node) {
+    Rewriter rewriter = {
+        .src_arena = NULL,
+        .dst_arena = arena,
+        .rewrite_fn = recreate_node_identity
+    };
+    return rewrite_node(&rewriter, node);
+}
+
+Nodes import_nodes(IrArena* dst_arena, Nodes old_nodes) {
+    size_t count = old_nodes.count;
+    const Node* arr[count];
+    for (size_t i = 0; i < count; i++)
+        arr[i] = import_node(dst_arena, old_nodes.nodes[i]);
+    return nodes(dst_arena, count, arr);
+}
+
+Strings import_strings(IrArena* dst_arena, Strings old_strings) {
+    size_t count = old_strings.count;
+    String arr[count];
+    for (size_t i = 0; i < count; i++)
+        arr[i] = string(dst_arena, old_strings.strings[i]);
+    return strings(dst_arena, count, arr);
+}
