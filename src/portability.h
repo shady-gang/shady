1f3befcd307ede4e5dccd623cafddfec5b79a7ed
Hugo Devillers <devillers@cg.uni-saarland.de>
2022-06-16 17:27:55 +0200

    avoid using VLAs because it's UB to have empty ones

--- src/portability.h
+++ src/portability.h
@@ -3,16 +3,19 @@
 
 #include <stdlib.h>
 
-
 #ifdef _MSC_VER
-#define SHADY_UNUSED
-#define LARRAY(T, name, size) T* name = alloca(sizeof(T) * (size))
-#define alloca _alloca
-#define SHADY_FALLTHROUGH
+    #define SHADY_UNUSED
+    #define LARRAY(T, name, size) T* name = alloca(sizeof(T) * (size))
+    #define alloca _alloca
+    #define SHADY_FALLTHROUGH
 #else
-#define LARRAY(T, name, size) T name[size]
-#define SHADY_UNUSED __attribute__((unused))
-#define SHADY_FALLTHROUGH __attribute__((fallthrough));
+    #ifdef USE_VLAS
+        #define LARRAY(T, name, size) T name[size]
+    #else
+        #define LARRAY(T, name, size) T* name = alloca(sizeof(T) * (size))
+    #endif
+    #define SHADY_UNUSED __attribute__((unused))
+    #define SHADY_FALLTHROUGH __attribute__((fallthrough));
 #endif
 
 #endif
