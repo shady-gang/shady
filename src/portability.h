07add416a516e188176091f12fe88ad4cb5a7ea9
Hugo Devillers <hugo@xol.io>
2022-07-26 16:04:03 +0200

    more silly hacks to allow MSVC to compile this

--- src/portability.h
+++ src/portability.h
@@ -3,11 +3,20 @@
 
 #include <stdlib.h>
 
+#include <assert.h>
+
+inline static void check_c11() {
+    static_assert(_CRT_HAS_C11, "C11 support is required to build shady.");
+}
+
 #ifdef _MSC_VER
     #define SHADY_UNUSED
     #define LARRAY(T, name, size) T* name = alloca(sizeof(T) * (size))
     #define alloca _alloca
     #define SHADY_FALLTHROUGH
+    // It's mid 2022, and this typedef is missing from <stdalign.h>
+    // MSVC is not a real C11 compiler.
+    typedef long long max_align_t;
 #else
     #ifdef USE_VLAS
         #define LARRAY(T, name, size) T name[size]
