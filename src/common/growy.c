725c107d9d772b1831380a8e5770be0543e274b1
Hugo Devillers <devillers@cg.uni-saarland.de>
2022-10-01 10:42:36 +0200

    added growy buffers

---
+++ src/common/growy.c
@@ -0,0 +1,45 @@
+#include "growy.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+static size_t init_size = 4096;
+
+struct Growy_ {
+    char* buffer;
+    size_t used, size;
+};
+
+Growy* new_growy() {
+    Growy* g = calloc(1, sizeof(Growy));
+    *g = (Growy) {
+        .buffer = calloc(1, init_size),
+        .size = init_size,
+        .used = 0
+    };
+    return g;
+}
+
+void growy_append_bytes(Growy* g, size_t s, char* bytes) {
+    size_t old_used = g->used;
+    g->used += s;
+    while (g->used >= g->size) {
+        g->size *= 2;
+        g->buffer = realloc(g->buffer, g->size);
+    }
+    memcpy(g->buffer + old_used, bytes, s);
+}
+
+void growy_destroy(Growy* g) {
+    free(g->buffer);
+    free(g);
+}
+
+char* growy_deconstruct(Growy* g) {
+    char* buf = g->buffer;
+    free(g);
+    return buf;
+}
+
+size_t growy_size(const Growy* g) { return g->used; }
+char* growy_data(const Growy* g) { return g->buffer; }
---
+++ src/common/growy.h
@@ -0,0 +1,19 @@
+#ifndef SHADY_GROWY_H
+#define SHADY_GROWY
+
+#include <stddef.h>
+
+/// Growy buffer, a buffer that can grow.
+/// Addresses not guaranteed to be stable.
+typedef struct Growy_ Growy;
+
+Growy* new_growy();
+void growy_append_bytes(Growy*, size_t, char*);
+#define growy_append(a, v) growy_append_bytes(a, sizeof(v), (char*) &v)
+size_t growy_size(const Growy*);
+char* growy_data(const Growy*);
+void growy_destroy(Growy*);
+// Like destroy, but we scavenge the internal allocation for later use.
+char* growy_deconstruct(Growy*);
+
+#endif
