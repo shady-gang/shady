263ef1388eae7289c65b137be51aa8bc7c13d3dc
Hugo Devillers <devillers@cg.uni-saarland.de>
2023-01-02 21:52:47 +0100

    simplified lower_mask further

--- src/shady/passes/lower_mask.c
+++ src/shady/passes/lower_mask.c
@@ -10,72 +10,59 @@
 
 typedef struct {
     Rewriter rewriter;
+    const Node* zero;
+    const Node* one;
 } Context;
-
-/// Removes most instructions that deal with masks and lower them to bitwise operations on integers
-const Node* process_let(Context* ctx, const Node* node) {
-    IrArena* arena = ctx->rewriter.dst_arena;
-
-    const Node* old_instruction = node->payload.let.instruction;
-    const Node* instruction = NULL;
-    const Node* tail = rewrite_node(&ctx->rewriter, node->payload.let.tail);
-
-    const Type* mask_type = get_actual_mask_type(ctx->rewriter.dst_arena);
-    assert(mask_type->tag == Int_TAG);
-
-    const Node* zero = int_literal(arena, (IntLiteral) { .width = mask_type->payload.int_type.width, .value = 0 });
-    const Node* one = int_literal(arena, (IntLiteral) { .width = mask_type->payload.int_type.width, .value = 1 });
-
-    if (old_instruction->tag == PrimOp_TAG) {
-        Op op = old_instruction->payload.prim_op.op;
-        Nodes old_nodes = old_instruction->payload.prim_op.operands;
-        switch(op) {
-            case empty_mask_op: {
-                instruction = quote_single(arena, zero);
-                break;
-            }
-            case mask_is_thread_active_op: {
-                BodyBuilder* bb = begin_body(ctx->rewriter.dst_module);
-                const Node* mask = rewrite_node(&ctx->rewriter, old_nodes.nodes[0]);
-                const Node* index = rewrite_node(&ctx->rewriter, old_nodes.nodes[1]);
-                index = gen_reinterpret_cast(bb, get_actual_mask_type(ctx->rewriter.dst_arena), index);
-                const Node* acc = mask;
-                // acc >>= index
-                acc = gen_primop_ce(bb, rshift_logical_op, 2, (const Node* []) { acc, index });
-                // acc &= 0x1
-                acc = gen_primop_ce(bb, and_op, 2, (const Node* []) { acc, one });
-                // acc == 1
-                acc = gen_primop_ce(bb, eq_op, 2, (const Node* []) { acc, one });
-                return finish_body(bb, let(arena, quote_single(arena, acc), tail));
-            }
-            case subgroup_active_mask_op:
-                // this is just ballot(true), lower it to that
-                instruction = prim_op(arena, (PrimOp) { .op = subgroup_ballot_op, .type_arguments = empty(arena), .operands = singleton(true_lit(ctx->rewriter.dst_arena)) });
-                break;
-            default: break;
-        }
-    }
-
-    if (!instruction)
-        instruction = rewrite_node(&ctx->rewriter, old_instruction);
-
-    return let(arena, instruction, tail);
-}
 
 const Node* process(Context* ctx, const Node* node) {
     const Node* found = search_processed(&ctx->rewriter, node);
     if (found) return found;
 
-    if (node->tag == MaskType_TAG)
-        return get_actual_mask_type(ctx->rewriter.dst_arena);
-    else if (node->tag == Let_TAG)
-        return process_let(ctx, node);
-    else return recreate_node_identity(&ctx->rewriter, node);
+    IrArena* arena = ctx->rewriter.dst_arena;
+
+    switch (node->tag) {
+        case MaskType_TAG: return get_actual_mask_type(ctx->rewriter.dst_arena);
+        case PrimOp_TAG: {
+            Op op = node->payload.prim_op.op;
+            Nodes old_nodes = node->payload.prim_op.operands;
+            switch(op) {
+                case empty_mask_op: return quote_single(arena, ctx->zero);
+                case subgroup_active_mask_op: // this is just ballot(true)
+                    return prim_op(arena, (PrimOp) { .op = subgroup_ballot_op, .type_arguments = empty(arena), .operands = singleton(true_lit(ctx->rewriter.dst_arena)) });
+                // extract the relevant bit
+                case mask_is_thread_active_op: {
+                    BodyBuilder* bb = begin_body(ctx->rewriter.dst_module);
+                    const Node* mask = rewrite_node(&ctx->rewriter, old_nodes.nodes[0]);
+                    const Node* index = rewrite_node(&ctx->rewriter, old_nodes.nodes[1]);
+                    index = gen_reinterpret_cast(bb, get_actual_mask_type(ctx->rewriter.dst_arena), index);
+                    const Node* acc = mask;
+                    // acc >>= index
+                    acc = gen_primop_ce(bb, rshift_logical_op, 2, (const Node* []) { acc, index });
+                    // acc &= 0x1
+                    acc = gen_primop_ce(bb, and_op, 2, (const Node* []) { acc, ctx->one });
+                    // acc == 1
+                    acc = gen_primop_ce(bb, eq_op, 2, (const Node* []) { acc, ctx->one });
+                    return yield_values_and_wrap_in_control(bb, singleton(acc));
+                }
+                default: break;
+            }
+            break;
+        }
+        default: break;
+    }
+
+    return recreate_node_identity(&ctx->rewriter, node);
 }
 
 void lower_mask(SHADY_UNUSED CompilerConfig* config, Module* src, Module* dst) {
+    IrArena* arena = dst->arena;
+    const Type* mask_type = get_actual_mask_type(arena);
+    assert(mask_type->tag == Int_TAG);
+
     Context ctx = {
         .rewriter = create_rewriter(src, dst, (RewriteFn) process),
+        .zero = int_literal(arena, (IntLiteral) { .width = mask_type->payload.int_type.width, .value = 0 }),
+        .one = int_literal(arena, (IntLiteral) { .width = mask_type->payload.int_type.width, .value = 1 }),
     };
     rewrite_module(&ctx.rewriter);
     destroy_rewriter(&ctx.rewriter);
