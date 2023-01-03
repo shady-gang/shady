0e4b2ad69280445a369aec151c08a37c292549a7
Hugo Devillers <devillers@cg.uni-saarland.de>
2023-01-03 13:54:55 +0100

    lower_subgroup_ops: no longer relies on implicit broadcasting on store

--- src/shady/passes/lower_subgroup_ops.c
+++ src/shady/passes/lower_subgroup_ops.c
@@ -37,8 +37,8 @@ static const Node* process_let(Context* ctx, const Node* old) {
         switch (payload.op) {
             case subgroup_broadcast_first_op: {
                 BodyBuilder* builder = begin_body(ctx->rewriter.dst_module);
-                const Node* operand = rewrite_node(&ctx->rewriter, payload.operands.nodes[0]);
-                const Type* element_type = get_unqualified_type(operand->type);
+                const Node* varying_value = rewrite_node(&ctx->rewriter, payload.operands.nodes[0]);
+                const Type* element_type = get_unqualified_type(varying_value->type);
 
                 if (element_type->tag == Int_TAG && element_type->payload.int_type.width == IntTy32) {
                     cancel_body(builder);
@@ -49,23 +49,31 @@ static const Node* process_let(Context* ctx, const Node* old) {
                 }
 
                 const Type* local_arr_ty = arr_type(arena, (ArrType) { .element_type = int32_type(arena), .size = int32_literal(arena, 2) });
-                const Type* ptr_t = ptr_type(arena, (PtrType) { .address_space = AsSubgroupPhysical, .pointed_type = local_arr_ty });
-                const Node* subgroup_arr_top = gen_primop_e(builder, get_stack_base_uniform_op, empty(arena), empty(arena));
 
-                const Node* local_array = gen_reinterpret_cast(builder, ptr_t, subgroup_arr_top);
-                const Type* elem_ptr_t = ptr_type(arena, (PtrType) { .address_space = AsSubgroupPhysical, .pointed_type = element_type });
-                const Node* ptr_elem = gen_reinterpret_cast(builder, elem_ptr_t, subgroup_arr_top);
+                const Node* varying_top_of_stack = gen_primop_e(builder, get_stack_base_op, empty(arena), empty(arena));
+                const Type* varying_raw_ptr_t = ptr_type(arena, (PtrType) { .address_space = AsPrivatePhysical, .pointed_type = local_arr_ty });
+                const Node* varying_raw_ptr = gen_reinterpret_cast(builder, varying_raw_ptr_t, varying_top_of_stack);
+                const Type* varying_typed_ptr_t = ptr_type(arena, (PtrType) { .address_space = AsPrivatePhysical, .pointed_type = element_type });
+                const Node* varying_typed_ptr = gen_reinterpret_cast(builder, varying_typed_ptr_t, varying_top_of_stack);
 
-                gen_store(builder, ptr_elem, operand);
+                const Node* uniform_top_of_stack = gen_primop_e(builder, get_stack_base_uniform_op, empty(arena), empty(arena));
+                const Type* uniform_raw_ptr_t = ptr_type(arena, (PtrType) { .address_space = AsSubgroupPhysical, .pointed_type = local_arr_ty });
+                const Node* uniform_raw_ptr = gen_reinterpret_cast(builder, uniform_raw_ptr_t, uniform_top_of_stack);
+                const Type* uniform_typed_ptr_t = ptr_type(arena, (PtrType) { .address_space = AsSubgroupPhysical, .pointed_type = element_type });
+                const Node* uniform_typed_ptr = gen_reinterpret_cast(builder, uniform_typed_ptr_t, uniform_top_of_stack);
+
+                gen_store(builder, varying_typed_ptr, varying_value);
                 for (int32_t j = 0; j < 2; j++) {
-                    const Node* logical_addr = gen_lea(builder, local_array, int32_literal(arena, 0), nodes(arena, 1, (const Node* []) { int32_literal(arena, j) }));
-                    const Node* input = gen_load(builder, logical_addr);
+                    const Node* varying_logical_addr = gen_lea(builder, varying_raw_ptr, int32_literal(arena, 0), nodes(arena, 1, (const Node* []) {int32_literal(arena, j) }));
+                    const Node* input = gen_load(builder, varying_logical_addr);
+
                     const Node* partial_result = gen_primop_ce(builder, subgroup_broadcast_first_op, 1, (const Node* []) { input });
-                    gen_store(builder, logical_addr, partial_result);
+
+                    const Node* uniform_logical_addr = gen_lea(builder, uniform_raw_ptr, int32_literal(arena, 0), nodes(arena, 1, (const Node* []) {int32_literal(arena, j) }));
+                    gen_store(builder, uniform_logical_addr, partial_result);
                 }
-                const Node* result = gen_load(builder, ptr_elem);
+                const Node* result = gen_load(builder, uniform_typed_ptr);
                 return finish_body(builder, let(arena, quote_single(arena, result), tail));
-                error("TODO")
             }
             default: break;
         }
