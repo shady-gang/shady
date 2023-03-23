2acbffc6d56b3c37d745706c3680389576986094
Hugo Devillers <devillers@cg.uni-saarland.de>
2023-03-23 16:57:32 +0100

    s2s: parse away basic things

--- src/s2s/s2s.c
+++ src/s2s/s2s.c
@@ -1,7 +1,19 @@
 #include "s2s.h"
+
+// this avoids polluting the namespace
+#define SpvHasResultAndType ShadySpvHasResultAndType
+
+// hacky nonsense because this guy is not declared static
+typedef enum SpvOp_ SpvOp;
+static void SpvHasResultAndType(SpvOp opcode, bool *hasResult, bool *hasResultType);
+
+#define SPV_ENABLE_UTILITY_CODE 1
 #include "spirv/unified1/spirv.h"
 
+#include "log.h"
+
 #include <assert.h>
+#include <stdlib.h>
 
 typedef struct {
     struct {
@@ -12,12 +24,21 @@ typedef struct {
 } SpvHeader;
 
 typedef struct {
+    enum { Todo, Str, Decl, Value } type;
+    union {
+        Node* node;
+        String str;
+    };
+} SpvDef;
+
+typedef struct {
     size_t cursor;
     size_t len;
     uint32_t* words;
     Module* mod;
 
     SpvHeader header;
+    SpvDef* defs;
 } SpvParser;
 
 bool parse_spv_header(SpvParser* parser) {
@@ -31,6 +52,49 @@ bool parse_spv_header(SpvParser* parser) {
     parser->header.generator = parser->words[2];
     parser->header.bound = parser->words[3];
     assert(parser->words[4] == 0);
+    parser->cursor = 5;
+    return true;
+}
+
+String decode_spv_string_literal(SpvParser* parser, uint32_t* at) {
+    // TODO: assumes little endian
+    return string(get_module_arena(parser->mod), (const char*) at);
+}
+
+bool parse_spv_instruction(SpvParser* parser) {
+    size_t available = parser->len - parser->cursor;
+    assert(available >= 1);
+    uint32_t* instruction = parser->words + parser->cursor;
+    SpvOp op = instruction[0] & 0xFFFF;
+    int size = (int) ((instruction[0] >> 16u) & 0xFFFFu);
+
+    SpvId result = 0;
+    bool has_result, has_type;
+    SpvHasResultAndType(op, &has_result, &has_type);
+    if (has_result)
+        result = instruction[1];
+
+    switch (op) {
+        // shady doesn't care, the emitter will set those up
+        case SpvOpMemoryModel:
+        case SpvOpCapability: break;
+        // these are basically just strings and we can infer how to handle them from ctx at the uses
+        case SpvOpName:
+        case SpvOpExtInstImport:
+        case SpvOpSource:
+        case SpvOpString: {
+            parser->defs[result].type = Str;
+            parser->defs[result].str = decode_spv_string_literal(parser, instruction + 2);
+            break;
+        }
+        // more debug nonsense
+        case SpvOpModuleProcessed:
+        case SpvOpSourceExtension:
+            break;
+        default: error("Unsupported op: %d, size: %d", op, size);
+    }
+
+    parser->cursor += size;
     return true;
 }
 
@@ -44,8 +108,12 @@ S2SError parse_spirv_into_shady(Module* dst, size_t len, uint32_t* words) {
 
     if (!parse_spv_header(&parser))
         return S2S_FailedParsingGeneric;
+    assert(parser.header.bound > 0 && parser.header.bound < 512 * 1024 * 1024); // sanity check
+    parser.defs = calloc(parser.header.bound, sizeof(SpvDef));
 
-    assert(false && "TODO");
+    while (parser.cursor < parser.len) {
+        parse_spv_instruction(&parser);
+    }
 
     return S2S_Success;
 }
