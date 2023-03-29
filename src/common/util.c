387db84c7dc2e24d96d7777d64cadb5acb9748ce
Michael Kenzel <michael.kenzel@gmail.com>
2023-03-29 19:20:06 +0200

    improved error handling for read_file

--- src/common/util.c
+++ src/common/util.c
@@ -3,18 +3,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+static long get_file_size(FILE* f) {
+    if (fseek(f, 0, SEEK_END) != 0)
+        return -1;
+
+    long fsize = ftell(f);
+
+    if (fsize == -1)
+        return -1;
+
+    if (fseek(f, 0, SEEK_SET) != 0)  /* same as rewind(f); */
+        return -1;
+
+    return fsize;
+}
+
 bool read_file(const char* filename, size_t* size, unsigned char** output) {
     FILE *f = fopen(filename, "rb");
     if (f == NULL)
         return false;
 
-    fseek(f, 0, SEEK_END);
-    long fsize = ftell(f);
-    fseek(f, 0, SEEK_SET);  /* same as rewind(f); */
+    long fsize = get_file_size(f);
+    if (fsize < 0)
+        goto err_post_open;
 
     // pad an extra zero at the end so this can be safely treated like a string
     unsigned char* string = malloc(fsize + 1);
-    fread(string, fsize, 1, f);
+    if (!string)
+        goto err_post_open;
+
+    if (fread(string, fsize, 1, f) != 1)
+        goto err_post_alloc;
+
     fclose(f);
 
     string[fsize] = 0;
@@ -23,6 +43,12 @@ bool read_file(const char* filename, size_t* size, unsigned char** output) {
     if (size)
         *size = fsize;
     return true;
+
+err_post_alloc:
+    free(string);
+err_post_open:
+    fclose(f);
+    return false;
 }
 
 void error_die() {
