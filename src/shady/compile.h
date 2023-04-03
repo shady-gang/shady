ddefde933c9e19bf289a335067b2068f5deed48e
Hugo Devillers <devillers@cg.uni-saarland.de>
2023-04-03 14:01:01 +0200

    only run verify_module in debug mode

--- src/shady/compile.h
+++ src/shady/compile.h
@@ -6,6 +6,12 @@
 #include "log.h"
 #include "analysis/verify.h"
 
+#ifdef NDEBUG
+#define SHADY_RUN_VERIFY 0
+#else
+#define SHADY_RUN_VERIFY 1
+#endif
+
 #define RUN_PASS(pass_name)                             \
 old_mod = mod;                                          \
 old_arena = tmp_arena;                                  \
@@ -14,6 +20,7 @@ mod = new_module(tmp_arena, get_module_name(old_mod));  \
 pass_name(config, old_mod, mod);                        \
 debug_print("After "#pass_name" pass: \n");             \
 log_module(DEBUG, config, mod);                         \
+if (SHADY_RUN_VERIFY)                                   \
 verify_module(mod);                                     \
 mod->sealed = true;                                     \
 if (old_arena) destroy_ir_arena(old_arena);
