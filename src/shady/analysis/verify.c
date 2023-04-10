f9d3671c9cf3b56fe8b5566e4a501224173e7210
Hugo Devillers <devillers@cg.uni-saarland.de>
2023-04-10 15:30:35 +0200

    added type verification to nominal nodes

--- src/shady/analysis/verify.c
+++ src/shady/analysis/verify.c
@@ -5,6 +5,7 @@
 
 #include "../visit.h"
 #include "../ir_private.h"
+#include "../type.h"
 
 #include "dict.h"
 #include "list.h"
@@ -64,10 +65,69 @@ static void verify_scoping(Module* mod) {
     destroy_list(scopes);
 }
 
+static void verify_nominal_node(const Node* fn, const Node* n) {
+    switch (n->tag) {
+        case Function_TAG: {
+            assert(!fn && "functions cannot be part of a scope, except as the entry");
+            break;
+        }
+        case BasicBlock_TAG: {
+            assert(is_subtype(noret_type(n->arena), n->payload.basic_block.body->type));
+            break;
+        }
+        case NominalType_TAG: {
+            assert(is_type(n->payload.nom_type.body));
+            break;
+        }
+        case Constant_TAG: {
+            const Type* t = n->payload.constant.value->type;
+            bool u = deconstruct_qualified_type(&t);
+            assert(u);
+            assert(is_subtype(n->payload.constant.type_hint, t));
+            break;
+        }
+        case GlobalVariable_TAG: {
+            if (n->payload.global_variable.init) {
+                const Type* t = n->payload.global_variable.init->type;
+                bool u = deconstruct_qualified_type(&t);
+                assert(u);
+                assert(is_subtype(n->payload.global_variable.type, t));
+            }
+            break;
+        }
+        default: break;
+    }
+}
+
+static void verify_bodies(Module* mod) {
+    struct List* scopes = build_scopes(mod);
+    for (size_t i = 0; i < entries_count_list(scopes); i++) {
+        Scope* scope = read_list(Scope*, scopes)[i];
+
+        for (size_t j = 0; j < scope->size; j++) {
+            CFNode* n = scope->rpo[j];
+            if (n->node->tag == BasicBlock_TAG) {
+                verify_nominal_node(scope->entry->node, n->node);
+            }
+        }
+
+        destroy_scope(scope);
+    }
+    destroy_list(scopes);
+
+    Nodes decls = get_module_declarations(mod);
+    for (size_t i = 0; i < decls.count; i++) {
+        const Node* decl = decls.nodes[i];
+        verify_nominal_node(NULL, decl);
+    }
+}
+
 void verify_module(Module* mod) {
     verify_same_arena(mod);
     // before we normalize the IR, scopes are broken because decls appear where they should not
     // TODO add a normalized flag to the IR and check grammar is adhered to strictly
-    if (get_module_arena(mod)->config.check_types)
-       verify_scoping(mod);
+    if (get_module_arena(mod)->config.check_types) {
+        verify_scoping(mod);
+        verify_bodies(mod);
+    }
 }
