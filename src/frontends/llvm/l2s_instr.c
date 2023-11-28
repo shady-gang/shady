1e4231b075eebff2bb4380feafa7fd603937dd64
Hugo Devillers <devillers@cg.uni-saarland.de>
2023-11-28 11:48:42 +0100

    l2s: have alloca create untyped pointers too (in untyped ptr mode)

--- src/frontends/llvm/l2s_instr.c
+++ src/frontends/llvm/l2s_instr.c
@@ -199,6 +199,10 @@ EmittedInstr convert_instruction(Parser* p, Node* fn_or_bb, BodyBuilder* b, LLVM
             const Type* allocated_t = convert_type(p, LLVMGetAllocatedType(instr));
             const Type* allocated_ptr_t = ptr_type(a, (PtrType) { .pointed_type = allocated_t, .address_space = AsPrivatePhysical });
             r = first(bind_instruction_explicit_result_types(b, prim_op_helper(a, alloca_op, singleton(allocated_t), empty(a)), singleton(allocated_ptr_t), NULL, false));
+            if (UNTYPED_POINTERS) {
+                const Type* untyped_ptr_t = ptr_type(a, (PtrType) { .pointed_type = unit_type(a), .address_space = AsPrivatePhysical });
+                r = first(bind_instruction_outputs_count(b, prim_op_helper(a, reinterpret_op, singleton(untyped_ptr_t), singleton(r)), 1, NULL, false));
+            }
             r = prim_op_helper(a, convert_op, singleton(t), singleton(r));
             break;
         }
