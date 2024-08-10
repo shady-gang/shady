df9521f39c2c94617ec9a0c35a36e7a01368e1f0
Hugo Devillers <hugo@xol.io>
2024-08-10 18:10:33 +0200

    fixed lower_subgroup_vars

--- src/shady/passes/lower_subgroup_vars.c
+++ src/shady/passes/lower_subgroup_vars.c
@@ -1,3 +1,5 @@
+#include <dict.h>
+
 #include "pass.h"
 
 #include "../type.h"
@@ -9,9 +11,10 @@
 typedef struct {
     Rewriter rewriter;
     const CompilerConfig* config;
+    BodyBuilder* bb;
 } Context;
 
-static const Node* process(Context* ctx, NodeClass class, String op_name, const Node* node) {
+static const Node* process(Context* ctx, const Node* node) {
     if (!node) return NULL;
     const Node* found = search_processed(&ctx->rewriter, node);
     if (found) return found;
@@ -20,6 +23,20 @@ static const Node* process(Context* ctx, NodeClass class, String op_name, const 
     IrArena* a = r->dst_arena;
 
     switch (node->tag) {
+        case Function_TAG: {
+            Node* newfun = recreate_decl_header_identity(r, node);
+            Context functx = *ctx;
+            functx.rewriter.map = clone_dict(functx.rewriter.map);
+            clear_dict(functx.rewriter.map);
+            register_processed_list(&functx.rewriter, get_abstraction_params(node), get_abstraction_params(newfun));
+            functx.bb = begin_body_with_mem(a, get_abstraction_mem(newfun));
+            Node* post_prelude = basic_block(a, empty(a), "post-prelude");
+            register_processed(&functx.rewriter, get_abstraction_mem(node), get_abstraction_mem(post_prelude));
+            set_abstraction_body(post_prelude, rewrite_node(&functx.rewriter, get_abstraction_body(node)));
+            set_abstraction_body(newfun, finish_body(functx.bb, jump_helper(a, post_prelude, empty(a), bb_mem(functx.bb))));
+            destroy_dict(functx.rewriter.map);
+            return newfun;
+        }
         case PtrType_TAG: {
             AddressSpace as = node->payload.ptr_type.address_space;
             if (as == AsSubgroup) {
@@ -31,30 +48,29 @@ static const Node* process(Context* ctx, NodeClass class, String op_name, const 
             const Node* odecl = node->payload.ref_decl.decl;
             if (odecl->tag != GlobalVariable_TAG || odecl->payload.global_variable.address_space != AsSubgroup)
                 break;
-            const Node* ndecl = rewrite_op(&ctx->rewriter, NcDeclaration, "decl", odecl);
-            error("TODO")
-            // assert(ctx->b);
-            // const Node* index = gen_builtin_load(ctx->rewriter.dst_module, ctx->b, BuiltinSubgroupId);
-            // const Node* slice = gen_lea(ctx->b, ref_decl_helper(a, ndecl), int32_literal(a, 0), mk_nodes(a, index));
-            // return slice;
+            const Node* ndecl = rewrite_node(&ctx->rewriter, odecl);
+            assert(ctx->bb);
+            const Node* index = gen_builtin_load(ctx->rewriter.dst_module, ctx->bb, BuiltinSubgroupId);
+            const Node* slice = gen_lea(ctx->bb, ref_decl_helper(a, ndecl), int32_literal(a, 0), mk_nodes(a, index));
+            return slice;
         }
         case GlobalVariable_TAG: {
             AddressSpace as = node->payload.global_variable.address_space;
             if (as == AsSubgroup) {
-                const Type* ntype = rewrite_op(&ctx->rewriter, NcType, "type", node->payload.global_variable.type);
+                const Type* ntype = rewrite_node(&ctx->rewriter, node->payload.global_variable.type);
                 const Type* atype = arr_type(a, (ArrType) {
                     .element_type = ntype,
-                    .size = ref_decl_helper(a, rewrite_op(&ctx->rewriter, NcDeclaration, "decl", get_declaration(ctx->rewriter.src_module, "SUBGROUPS_PER_WG")))
+                    .size = ref_decl_helper(a, rewrite_node(&ctx->rewriter, get_declaration(ctx->rewriter.src_module, "SUBGROUPS_PER_WG")))
                 });
 
                 assert(lookup_annotation(node, "Logical") && "All subgroup variables should be logical by now!");
-                Node* new = global_var(ctx->rewriter.dst_module, rewrite_ops(&ctx->rewriter, NcAnnotation, "annotations", node->payload.global_variable.annotations), atype, node->payload.global_variable.name, AsShared);
+                Node* new = global_var(ctx->rewriter.dst_module, rewrite_nodes(&ctx->rewriter, node->payload.global_variable.annotations), atype, node->payload.global_variable.name, AsShared);
                 register_processed(&ctx->rewriter, node, new);
 
                 if (node->payload.global_variable.init) {
                     new->payload.global_variable.init = fill(a, (Fill) {
                         .type = atype,
-                        .value = rewrite_op(&ctx->rewriter, NcValue, "init", node->payload.global_variable.init)
+                        .value = rewrite_node(&ctx->rewriter, node->payload.global_variable.init)
                     });
                 }
                 return new;
@@ -64,10 +80,13 @@ static const Node* process(Context* ctx, NodeClass class, String op_name, const 
         default: break;
     }
 
-    const Node* new = recreate_node_identity(&ctx->rewriter, node);
-    if (is_instruction(new))
-        register_processed(r, node, new);
-    return new;
+    if (is_declaration(node)) {
+        Context declctx = *ctx;
+        declctx.bb = NULL;
+        return recreate_node_identity(&declctx.rewriter, node);
+    }
+
+    return recreate_node_identity(&ctx->rewriter, node);
 }
 
 Module* lower_subgroup_vars(const CompilerConfig* config, Module* src) {
@@ -75,7 +94,7 @@ Module* lower_subgroup_vars(const CompilerConfig* config, Module* src) {
     IrArena* a = new_ir_arena(&aconfig);
     Module* dst = new_module(a, get_module_name(src));
     Context ctx = {
-        .rewriter = create_op_rewriter(src, dst, (RewriteOpFn) process),
+        .rewriter = create_node_rewriter(src, dst, (RewriteNodeFn) process),
         .config = config
     };
     rewrite_module(&ctx.rewriter);
