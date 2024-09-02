bda535c2d45eda4990eaabf262834e91102ee9b9
Hugo Devillers <hugo@xol.io>
2024-09-02 10:10:20 +0200

    mem2reg: allow searching in dominators

--- src/shady/passes/opt_mem2reg.c
+++ src/shady/passes/opt_mem2reg.c
@@ -1,14 +1,18 @@
+#include <list.h>
+
 #include "shady/pass.h"
 
 #include "../ir_private.h"
 #include "../type.h"
 #include "../transform/ir_gen_helpers.h"
+#include "../analysis/cfg.h"
 
 #include "log.h"
 #include "portability.h"
 
 typedef struct {
     Rewriter rewriter;
+    CFG* cfg;
     bool* todo;
 } Context;
 
@@ -41,9 +45,18 @@ static const Node* get_ptr_source(const Node* ptr) {
     }
 }
 
-static const Node* get_last_stored_value(const Node* ptr, const Node* mem) {
+static const Node* get_last_stored_value(Context* ctx, const Node* ptr, const Node* mem) {
     while (mem) {
         switch (mem->tag) {
+            case AbsMem_TAG: {
+                const Node* abs = mem->payload.abs_mem.abs;
+                CFNode* n = cfg_lookup(ctx->cfg, abs);
+                if (entries_count_list(n->pred_edges) == 1) {
+                    CFEdge e = read_list(CFEdge, n->pred_edges)[0];
+                    mem = get_terminator_mem(e.terminator);
+                    continue;
+                }
+            }
             case Store_TAG: {
                 Store payload = mem->payload.store;
                 if (payload.ptr == ptr)
@@ -60,13 +73,21 @@ static const Node* process(Context* ctx, const Node* node) {
     Rewriter* r = &ctx->rewriter;
     IrArena* a = r->dst_arena;
     switch (node->tag) {
+        case Function_TAG: {
+            Node* new = recreate_decl_header_identity(r, node);
+            Context fun_ctx = *ctx;
+            fun_ctx.cfg = build_fn_cfg(node);
+            recreate_decl_body_identity(&fun_ctx.rewriter, node, new);
+            destroy_cfg(fun_ctx.cfg);
+            return new;
+        }
         case Load_TAG: {
             Load payload = node->payload.load;
             const Node* src = get_ptr_source(payload.ptr);
             if (src->tag != StackAlloc_TAG)
                 break;
             // for now, only simplify loads from non-leaking allocas
-            const Node* ovalue = get_last_stored_value(payload.ptr, payload.mem);
+            const Node* ovalue = get_last_stored_value(ctx, payload.ptr, payload.mem);
             if (ovalue) {
                 *ctx->todo = true;
                 const Node* value = rewrite_node(r, ovalue);
