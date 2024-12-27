49c0a942404ce38143b70fdf53f85657941bd9d8
Hugo Devillers <hugo@xol.io>
2024-12-27 17:47:06 +0100

    remove useless code in compile.c

--- src/shady/compile.c
+++ src/shady/compile.c
@@ -1,16 +1,10 @@
 #include "ir_private.h"
 #include "shady/driver.h"
-#include "shady/ir.h"
 
 #include "passes/passes.h"
 #include "analysis/verify.h"
 
-#include "transform/internal_constants.h"
-
-#include "util.h"
 #include "log.h"
-
-#include <stdbool.h>
 
 #ifdef NDEBUG
 #define SHADY_RUN_VERIFY 0
@@ -36,8 +30,6 @@ void shd_run_pass_impl(const CompilerConfig* config, Module** pmod, RewritePass 
         shd_verify_module(config, *pmod);
     if (shd_module_get_arena(old_mod) != shd_module_get_arena(*pmod))
         shd_destroy_ir_arena(shd_module_get_arena(old_mod));
-    // if (config->hooks.after_pass.fn)
-    //     config->hooks.after_pass.fn(config->hooks.after_pass.uptr, pass_name, *pmod);
 }
 
 void shd_apply_opt_impl(const CompilerConfig* config, bool* todo, Module** m, OptPass pass, String pass_name) {
@@ -49,87 +41,3 @@ void shd_apply_opt_impl(const CompilerConfig* config, bool* todo, Module** m, Op
         shd_log_module(DEBUGVV, config, *m);
     }
 }
-
-/*CompilationResult shd_run_compiler_passes(CompilerConfig* config, Module** pmod) {
-    IrArena* initial_arena = (*pmod)->arena;
-
-    // we don't want to mess with the original module
-    *pmod = shd_import(config, *pmod);
-    shd_log_fmt(DEBUG, "After import:\n");
-    shd_log_module(DEBUG, config, *pmod);
-
-    if (config->input_cf.has_scope_annotations) {
-        // RUN_PASS(shd_pass_scope_heuristic)
-        RUN_PASS(shd_pass_lift_everything)
-        RUN_PASS(shd_pass_scope2control)
-    } else if (config->input_cf.restructure_with_heuristics) {
-        RUN_PASS(shd_pass_remove_critical_edges)
-        RUN_PASS(shd_pass_lcssa)
-        // RUN_PASS(shd_pass_lift_everything)
-        RUN_PASS(shd_pass_reconvergence_heuristics)
-    }
-
-    if (config->dynamic_scheduling) {
-        add_scheduler_source(config, *pmod);
-    }
-
-    RUN_PASS(shd_pass_eliminate_inlineable_constants)
-
-    RUN_PASS(shd_pass_setup_stack_frames)
-    if (!config->hacks.force_join_point_lifting)
-        RUN_PASS(shd_pass_mark_leaf_functions)
-
-    RUN_PASS(shd_pass_lower_callf)
-    RUN_PASS(shd_pass_inline)
-
-    RUN_PASS(shd_pass_lift_indirect_targets)
-
-    RUN_PASS(shd_pass_specialize_execution_model)
-
-    //RUN_PASS(shd_pass_opt_stack)
-
-    if (config->specialization.entry_point)
-        RUN_PASS(shd_pass_specialize_entry_point)
-
-    RUN_PASS(shd_pass_add_init_fini)
-
-    RUN_PASS(shd_pass_lower_tailcalls)
-    RUN_PASS(shd_pass_lower_mask)
-    //RUN_PASS(shd_pass_lower_switch_btree)
-    //RUN_PASS(shd_pass_opt_mem2reg)
-    RUN_PASS(shd_pass_lower_subgroup_ops)
-
-    RUN_PASS(shd_pass_promote_io_variables)
-    RUN_PASS(shd_pass_lower_logical_pointers)
-
-    if (config->lower.emulate_physical_memory) {
-        RUN_PASS(shd_pass_lower_alloca)
-    }
-    RUN_PASS(shd_pass_lower_stack)
-    RUN_PASS(shd_pass_lower_memcpy)
-    RUN_PASS(shd_pass_lower_lea)
-    RUN_PASS(shd_pass_lower_generic_globals)
-    if (config->lower.emulate_generic_ptrs) {
-        RUN_PASS(shd_pass_lower_generic_ptrs)
-    }
-    if (config->lower.emulate_physical_memory) {
-        RUN_PASS(shd_pass_lower_physical_ptrs)
-    }
-    RUN_PASS(shd_pass_lower_subgroup_vars)
-    RUN_PASS(shd_pass_lower_memory_layout)
-
-    if (config->lower.decay_ptrs)
-        RUN_PASS(shd_pass_lower_decay_ptrs)
-
-    RUN_PASS(shd_pass_lower_int)
-
-    RUN_PASS(shd_pass_lower_fill)
-    RUN_PASS(shd_pass_lower_nullptr)
-    RUN_PASS(shd_pass_normalize_builtins)
-
-    RUN_PASS(shd_pass_restructurize)
-
-    return CompilationNoError;
-}*/
-
-#undef mod
