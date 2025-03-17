b362d4179f05e2740e7b2c4f18b014b0fe7f4115
Hugo Devillers <devillers@cg.uni-saarland.de>
2025-03-17 11:23:08 +0100

    testing: use test name for output spv

--- test/test_with_val.cmake
+++ test/test_with_val.cmake
@@ -1,7 +1,7 @@
-execute_process(COMMAND ${COMPILER} ${FILES} ${TARGS} -o ${DST}/${T}.spv COMMAND_ERROR_IS_FATAL ANY COMMAND_ECHO STDOUT)
+execute_process(COMMAND ${COMPILER} ${FILES} ${TARGS} -o ${DST}/${NAME}.spv COMMAND_ERROR_IS_FATAL ANY COMMAND_ECHO STDOUT)
 
 find_program(SPIRV_VALIDATOR "spirv-val")
 if (SPIRV_VALIDATOR)
     message("Validating stuff ${SPIRV_VALIDATOR}")
-    execute_process(COMMAND ${SPIRV_VALIDATOR} ${DST}/${T}.spv --target-env vulkan1.3 COMMAND_ERROR_IS_FATAL ANY)
+    execute_process(COMMAND ${SPIRV_VALIDATOR} ${DST}/${NAME}.spv --target-env vulkan1.3 COMMAND_ERROR_IS_FATAL ANY)
 endif ()
