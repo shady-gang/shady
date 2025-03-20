795a2a199a9f3670daa951f5a38fa163225e758d
Hugo Devillers <devillers@cg.uni-saarland.de>
2025-03-20 16:16:43 +0100

    added SHD_LOG_LEVEL envvar

--- src/common/log.c
+++ src/common/log.c
@@ -1,10 +1,36 @@
 #include "log.h"
 
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+static LogLevel default_log_level = INFO;
+static bool log_level_init = false;
 LogLevel shady_log_level = INFO;
 
 LogLevel shd_log_get_level(void) {
+    if (!log_level_init) {
+        shady_log_level = default_log_level;
+        char* env_level = getenv("SHD_LOG_LEVEL");
+        if (env_level) {
+            if (strcmp(env_level, "ERROR") == 0)
+                shady_log_level = ERROR;
+            if (strcmp(env_level, "INFO") == 0)
+                shady_log_level = INFO;
+            if (strcmp(env_level, "WARN") == 0)
+                shady_log_level = WARN;
+            if (strcmp(env_level, "INFO") == 0)
+                shady_log_level = INFO;
+            if (strcmp(env_level, "DEBUG") == 0)
+                shady_log_level = DEBUG;
+            if (strcmp(env_level, "DEBUGV") == 0)
+                shady_log_level = DEBUGVV;
+            if (strcmp(env_level, "DEBUGVV") == 0)
+                shady_log_level = DEBUGVV;
+        }
+        log_level_init = true;
+    }
     return shady_log_level;
 }
 
