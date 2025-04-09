8c0ab487adde3880964a63838a0d0f0e8b379510
Hugo Devillers <devillers@cg.uni-saarland.de>
2025-04-09 15:22:56 +0200

    cuda: added offsetof() polyfill

--- src/backend/c/prelude.cu
+++ src/backend/c/prelude.cu
@@ -1,2 +1,4 @@
 #define __shady_make_thread_local(var) var[__shady_workgroup_size]
 #define __shady_thread_local_access(var) (var[((threadIdx.x * blockDim.y + threadIdx.y) * blockDim.z + threadIdx.z)])
+
+#define offsetof(T, f) (long long int) &((T*)nullptr)->f
