073603a78c69612f54699f7938e359c76f340173
Gobrosse <hugo@xol.io>
2025-07-06 09:43:31 +0200

    rejigged tests

--- test/test_with_val.cmake
+++
@@ -1,7 +0,0 @@
-execute_process(COMMAND ${COMPILER} ${FILES} ${TARGS} -o ${DST}/${NAME}.spv COMMAND_ERROR_IS_FATAL ANY COMMAND_ECHO STDOUT)
-
-find_program(SPIRV_VALIDATOR "spirv-val")
-if (SPIRV_VALIDATOR)
-    message("Validating stuff ${SPIRV_VALIDATOR}")
-    execute_process(COMMAND ${SPIRV_VALIDATOR} ${DST}/${NAME}.spv --target-env vulkan1.3 COMMAND_ERROR_IS_FATAL ANY)
-endif ()
---
+++ test/unit_test.cmake
@@ -0,0 +1,9 @@
+execute_process(COMMAND ${COMPILER} ${FILES} ${COMP_ARGS} -o ${DST}/${NAME}.spv COMMAND_ERROR_IS_FATAL ANY COMMAND_ECHO STDOUT)
+
+if (VALSPV)
+    #find_program(SPIRV_VALIDATOR "spirv-val")
+    if (SPIRV_VALIDATOR)
+        #message("Validating stuff ${SPIRV_VALIDATOR} (args) ${SPV_VAL_ARGS}")
+        execute_process(COMMAND ${SPIRV_VALIDATOR} ${DST}/${NAME}.spv ${SPV_VAL_ARGS} COMMAND_ERROR_IS_FATAL ANY)
+    endif ()
+endif ()
