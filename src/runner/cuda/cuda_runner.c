ca897e07777d77f61ca22953925fcb1854d2f8b6
Hugo Devillers <devillers@cg.uni-saarland.de>
2025-11-10 17:21:58 +0100

    fix cuda 13

--- src/runner/cuda/cuda_runner.c
+++ src/runner/cuda/cuda_runner.c
@@ -99,7 +99,11 @@ static CudaDevice* create_cuda_device(CudaBackend* b, int ordinal) {
     CHECK_CUDA(cuDeviceGetName(device->name, 255, handle), goto dealloc_and_return_null);
     CHECK_CUDA(cuDeviceGetAttribute(&device->cc_major, CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MAJOR, device->handle), goto dealloc_and_return_null);
     CHECK_CUDA(cuDeviceGetAttribute(&device->cc_minor, CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MINOR, device->handle), goto dealloc_and_return_null);
+#if CUDA_VERSION >= 13
+    CHECK_CUDA(cuCtxCreate(&device->context, 0, 0, handle), goto dealloc_and_return_null);
+#else
     CHECK_CUDA(cuCtxCreate(&device->context, 0, handle), goto dealloc_and_return_null);
+#endif
     return device;
 
     dealloc_and_return_null:
