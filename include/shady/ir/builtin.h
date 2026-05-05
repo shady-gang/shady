f3babcc1954e4e4c2c9acd5951fe8ebb2bb2df26
Matthias Kurtenacker <matthias.kurtenacker@dfki.de>
2026-05-05 15:05:29 +0200

    Fix TriangleIndicesEXT array size to 1.
    This is a bugfix, we need a size to generate the memory layout.

--- include/shady/ir/builtin.h
+++ include/shady/ir/builtin.h
@@ -31,7 +31,7 @@ BUILTIN(WorkgroupSize,               AsUInput, Device,     shd_u32vec3_type(aren
 BUILTIN(NumSubgroups,                AsUInput, Invocation, shd_uint32_type(arena)  )\
 BUILTIN(NumWorkgroups,               AsUInput, Device,     shd_u32vec3_type(arena) )\
 BUILTIN(Position,                    AsOutput, Invocation, shd_f32vec4_type(arena) )\
-BUILTIN(PrimitiveTriangleIndicesEXT, AsOutput, Invocation, arr_type_helper(arena, 0, shd_u32vec3_type(arena), NULL) )\
+BUILTIN(PrimitiveTriangleIndicesEXT, AsOutput, Invocation, arr_type_helper(arena, 0, shd_u32vec3_type(arena), shd_uint32_literal(arena, 1)) )\
 BUILTIN(PrimitiveId,                 AsInput,  Invocation, shd_uint32_type(arena)  )\
 BUILTIN(SubgroupLocalInvocationId,   AsInput,  Invocation, shd_uint32_type(arena)  )\
 BUILTIN(SubgroupId,                  AsUInput, Subgroup,   shd_uint32_type(arena)  )\
